28dbcfbacbfcd33226999706bc50885ec9b008d6
Lenovo <Lenovo@DESKTOP-75SO5O4>
2022-11-07 13:56:24 +0100

    init

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi bilo koji broj od 1 do 3");
+            int n= int.Parse(Console.ReadLine());
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("*");
+                break;
+
+                case 2:
+                    Console.WriteLine("**");
+                break;
+
+                case 3:
+                    Console.WriteLine("***");
+                 break;
+                    default: Console.WriteLine("Niste uneli nijedan broj od 1 do 3");
+                    break;
+                   
+            }
+            Console.WriteLine("Pritisni ENTER za izlazak");
+            Console.ReadLine();
+        }
+    }
+}
